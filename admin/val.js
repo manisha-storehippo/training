62643c98fded253f841c2a0813884850e69fcd17
Manisha <manisha.kirodiwal@hippoinnovations.com>
2015-07-02 11:23:49 +0530

    .

--- admin/val.js
+++ admin/val.js
@@ -1,6 +1,7 @@
 /**
  * Created by hippoinnovations on 30/6/15.
  */
+
 function formValidation(){
     var uname=document.registration.fname;
     var uname1=document.registration.lname;
@@ -66,7 +67,7 @@ function passid_validation(passid,mx,my)
     var passid_len = passid.value.length;
     if (passid_len == 0 ||passid_len >= my || passid_len < mx)
     {
-        alert("Password should not be empty / length be between "+mx+" to "+my);
+        alert("Password should not be empty / length between "+mx+" to "+my);
         passid.focus();
         return false;
     }
